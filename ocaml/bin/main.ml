1dc16c958f1fe97e7dadfb81ddf3ea144970f60e
Nick Robison <nick@nickrobison.com>
2022-02-16 09:14:37 -0800

    Format Ocaml code
    Add ocamlformat file

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,32 +1,37 @@
-open  Lwt.Infix
+open Lwt.Infix
 
-let src = Logs.Src.create "Conductor" ~doc: "HomeOS Conductor"
+let src = Logs.Src.create "Conductor" ~doc:"HomeOS Conductor"
 
-module Log = (val Logs.src_log src: Logs.LOG)
+module Log = (val Logs.src_log src : Logs.LOG)
 
 let () =
-Logs.set_level (Some Logs.Info);
-Logs.set_reporter (Logs_fmt.reporter ())
+  Logs.set_level (Some Logs.Info);
+  Logs.set_reporter (Logs_fmt.reporter ())
 
 let secret_key = `Ephemeral
 let listen_address = `TCP ("127.0.0.1", 7000)
 
 let start_server =
-Lwt_main.run begin
-print_endline "Running";
-let config = Capnp_rpc_unix.Vat_config.create ~serve_tls:false ~secret_key listen_address in
-let service_id = Capnp_rpc_unix.Vat_config.derived_id config "registrar" in
-let restore = Capnp_rpc_net.Restorer.single service_id Conductor__.Registrar.local in
-Capnp_rpc_unix.serve config ~restore >>= fun vat ->
-let uri = Capnp_rpc_unix.Vat.sturdy_uri vat service_id in
-Log.info (fun m -> m"Services running on: %a@." Uri.pp_hum uri);
-fst @@ Lwt.wait ()
-end
+  Lwt_main.run
+    (print_endline "Running";
+     let config =
+       Capnp_rpc_unix.Vat_config.create ~serve_tls:false ~secret_key
+         listen_address
+     in
+     let service_id = Capnp_rpc_unix.Vat_config.derived_id config "registrar" in
+     let restore =
+       Capnp_rpc_net.Restorer.single service_id Conductor__.Registrar.local
+     in
+     Capnp_rpc_unix.serve config ~restore >>= fun vat ->
+     let uri = Capnp_rpc_unix.Vat.sturdy_uri vat service_id in
+     Log.info (fun m -> m "Services running on: %a@." Uri.pp_hum uri);
+     fst @@ Lwt.wait ())
 
 open Cmdliner
 
 let serve_cmd =
-Term.(const start_server), let doc = "run server" in
-Term.info "serve" ~doc
+  ( Term.(const start_server),
+    let doc = "run server" in
+    Term.info "serve" ~doc )
 
 let () = Term.eval serve_cmd |> Term.exit
